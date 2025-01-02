07ffa05ba724673105a9be187413347b25a18a4f
vosmiic <44092756+vosmiic@users.noreply.github.com>
2025-01-02 21:09:03 +0000

    adding room to db

--- VideoStreamShare/Controllers/RoomController.cs
+++ VideoStreamShare/Controllers/RoomController.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using VideoStreamShare.Data;
 
 namespace VideoStreamShare.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class RoomController : Controller {
+    private readonly ApplicationDbContext _context;
+
+    public RoomController(ApplicationDbContext context) {
+        _context = context;
+    }
 
     [HttpPost]
     public string CreateRoom([FromBody] string roomName) {
